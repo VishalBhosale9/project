d9d64a9af9d2c493dfe71f57b00434bb126f5c13
vishal <bhosalevishal510@gmail.com>
2022-06-06 11:11:18 +0530

    added personal project

---
+++ src/app/weatherforecast/weatherforecast.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SearchServiceService } from 'src/Auth/Weathersearch-service.service';
+
+@Component({
+  selector: 'app-weatherforecast',
+  templateUrl: './weatherforecast.component.html',
+  styleUrls: ['./weatherforecast.component.css']
+})
+export class WeatherforecastComponent implements OnInit {
+
+  cities: any = [];
+  totalResults: any;
+  favColor = "red"
+  found = false;
+  loading= true;
+  flag = false;
+  citieslist: any=[];
+  forecastday:any[]=[]
+  city: any;
+  days:string=''
+  constructor(public searchService: SearchServiceService, private snack: MatSnackBar) { }
+  ngDoCheck(): void {
+    
+  }
+
+  ngOnInit(): void 
+  { 
+  
+  }
+
+  fetchWeatherForecast(){
+
+    this.searchService.GetWeatherDetails(this.city,this.days).subscribe((resp:any)=> {
+ // this.forecast=response.forecast.forcastday;
+      console.log("---")
+      console.log(resp.forecast.forecastday)
+      
+this.forecastday=resp.forecast.forecastday;
+
+      this.ngOnInit();
+  
+      this.snack.open("Loaded", "OK",{duration:2000,
+        panelClass: ['mat-toolbar', 'mat-Success']});
+      
+  
+    },
+      (      error: { error: string; }) => {
+  
+         console.log("--"+error.error)
+         this.snack.open("Something went wrong", "OK",{duration:4000,
+          panelClass: ['mat-toolbar', 'mat-success']})
+          this.refresh();
+      }
+    );
+  }
+
+
+
+refresh(): void {
+  window.location.reload();
+}
+getDate(): string
+{
+  const timeElapsed = Date.now();
+  const today = new Date(timeElapsed);
+  return today.toDateString();
+}
+
+
+}
